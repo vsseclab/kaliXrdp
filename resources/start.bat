7ed7bd4aa5250276b24fcec850550cdfc504605b
VssecLab <91882824+vsseclab@users.noreply.github.com>
2024-03-15 22:01:10 +0700

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,16 +1,16 @@
 echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By Vss" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By vssl" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net localgroup administrators Vss /add >nul
-net user Vss /active:yes >nul
+net localgroup administrators vssl /add >nul
+net user vssl /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant Vss:F >nul
-ICACLS C:\Windows\installer /grant Vss:F >nul
+ICACLS C:\Windows\Temp /grant vssl:F >nul
+ICACLS C:\Windows\installer /grant vssl:F >nul
 echo Successfully installed!
 echo IP:
 tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
